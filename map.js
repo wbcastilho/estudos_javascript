0054f936624971bb8f04fb7bebf455c79a59f270
wbcastilho <welber.castilho@yahoo.com.br>
2021-02-10 22:33:39 -0300

    Adicionado arquivo map.js

---
+++ map.js
@@ -0,0 +1,43 @@
+/*---------------------------------------
+EXEMPLO 01
+-----------------------------------------*/
+const numbers = [1, 4, 5, 9, 14, 23];
+
+const doubledNumbers = numbers.map(function(elem){
+    return elem * 2;
+});
+
+console.log(doubleNumbers);
+
+/*---------------------------------------
+EXEMPLO 02
+-----------------------------------------*/
+const numbers = [1, 4, 5, 9, 14, 23];
+
+function doubleNumber(num){
+    return num * 2;
+}
+
+const doubledNumbers = numbers.map(doubleNumber);   
+
+console.log(doubledNumbers);
+
+/*---------------------------------------
+EXEMPLO 03
+-----------------------------------------*/
+const numbers = [1, 4, 5, 9, 14, 23];
+
+const doubledNumbers = numbers.map(num => num * 2);   
+
+console.log(doubledNumbers);
+
+/*---------------------------------------
+EXEMPLO 04
+-----------------------------------------*/
+const fahrenheit = [0, 32, 45, 50, 75, 120];
+
+const celsius = fahrenheit.map(function(elem){
+    return Math.round((elem - 32) * 5/9);
+});
+
+console.log(celsius);
